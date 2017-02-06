d4f18622729c188d6cd5f47858b9cacecdacedba
Ted Hansen <ted@detroitlabs.com>
2017-02-05 21:47:05 -0500

    rename tankDrive command to manualDrive minimal autonomous mode that drives in a straight line based on gyro feedback

--- src/org/usfirst/frc4680/Eagle/commands/AutonomousCommand.java
+++ src/org/usfirst/frc4680/Eagle/commands/AutonomousCommand.java
@@ -40,6 +40,7 @@ public class AutonomousCommand extends Command {
 
     // Called repeatedly when this Command is scheduled to run
     protected void execute() {
+    	Robot.drive.driveStraight(0.5, 0.0);
     }
 
     // Make this return true when this Command no longer needs to run execute()
@@ -49,6 +50,7 @@ public class AutonomousCommand extends Command {
 
     // Called once after isFinished returns true
     protected void end() {
+    	Robot.drive.driveStraight(0, 0);
     }
 
     // Called when another command which requires one or more of the same
