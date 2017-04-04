090a31aa592ff87a2e03e40befe7d0a2de988d52
Ted Hansen <ted@detroitlabs.com>
2017-04-04 14:03:15 -0400

    set drive system back to normal working arduino code for Pixy Camera

--- FRC_Pixy_Vision/FRC_Pixy_Vision.ino
+++ FRC_Pixy_Vision/FRC_Pixy_Vision.ino
@@ -1,27 +1,23 @@
 #include <Pixy.h>
-#include <PixyI2C.h>
-#include <PixySPI_SS.h>
-#include <PixyUART.h>
-#include <TPixy.h>
 #include <SPI.h>
 
-const int INVALID_TARGETS = 3;
-const int TURN_LEFT = 4;
-const int TURN_RIGHT = 5;
-const int CENTERED = 6;
-const int SMALL_TURN = 7;
+const int INVALID_TARGETS = 2;
+const int TURN_LEFT       = 3;
+const int CENTERED        = 4;
+const int TURN_RIGHT      = 5;
+const int SMALL_TURN      = 6;
 
-const int MAX_X_AXIS_FOR_SMALL_TURN_RIGHT = 155;
-const int MAX_X_AXIS_FOR_BIG_TURN_RIGHT = 145;
-const int MAX_X_AXIS_FOR_SMALL_TURN_LEFT = 165;
-const int MAX_X_AXIS_FOR_BIG_TURN_LEFT = 175;
+const int MAX_X_AXIS_FOR_BIG_TURN_RIGHT   = 165;
+const int MAX_X_AXIS_FOR_SMALL_TURN_RIGHT = 162;
+const int MAX_X_AXIS_FOR_SMALL_TURN_LEFT  = 158;
+const int MAX_X_AXIS_FOR_BIG_TURN_LEFT    = 155;
 
 Pixy pixy;
 int leftBlockIndex;
 int rightBlockIndex;
 
 void setup() {
-  Serial.begin(9600);
+  Serial.begin(115200);
 
   pixy.init();
 
@@ -37,14 +33,22 @@ void setup() {
 
 void loop() {
   uint16_t numOfBlocks;
-
+  delay(500);
   numOfBlocks = pixy.getBlocks();
   Serial.print(numOfBlocks);
   Serial.println(" blocks found");
+  for(int idx=0; idx<numOfBlocks; idx++) {
+    pixy.blocks[idx].print();
+  }
 
   if (numOfBlocks == 0 || numOfBlocks == 1) {
     digitalWrite(INVALID_TARGETS, HIGH);
-    leftBlockIndex = -1;
+    digitalWrite(TURN_LEFT, LOW);
+    digitalWrite(TURN_RIGHT, LOW);
+    digitalWrite(CENTERED, LOW);
+    digitalWrite(SMALL_TURN, LOW);
+    Serial.println("INVALID_TARGETS");
+     leftBlockIndex = -1;
     rightBlockIndex = -1;
     return;
   }
@@ -52,21 +56,17 @@ void loop() {
   if (numOfBlocks >= 2) {
     sortBlocks(numOfBlocks);
 
-    Serial.print(pixy.blocks[leftBlockIndex].x);
-    Serial.print(" ");
-    Serial.print(pixy.blocks[rightBlockIndex].x);
-    Serial.println();
-
-    if (areBlocksMismatched()) {
-      digitalWrite(INVALID_TARGETS, HIGH);
-      digitalWrite(TURN_LEFT, LOW);
-      digitalWrite(TURN_RIGHT, LOW);
-      digitalWrite(CENTERED, LOW);
-      digitalWrite(SMALL_TURN, LOW);
-      leftBlockIndex = -1;
-      rightBlockIndex = -1;
-      return;
-    }
+//    if (areBlocksMismatched()) {
+//      digitalWrite(INVALID_TARGETS, HIGH);
+//      digitalWrite(TURN_LEFT, LOW);
+//      digitalWrite(TURN_RIGHT, LOW);
+//      digitalWrite(CENTERED, LOW);
+//      digitalWrite(SMALL_TURN, LOW);
+//      Serial.println("INVALID_TARGETS");
+//      leftBlockIndex = -1;
+//      rightBlockIndex = -1;
+//      return;
+//    }
 
     sendCommand();
   }
@@ -111,36 +111,27 @@ boolean areBlocksMismatched() {
 void sendCommand() {
   int averageX = (pixy.blocks[leftBlockIndex].x + pixy.blocks[rightBlockIndex].x) / 2;
 
-  if (averageX > MAX_X_AXIS_FOR_BIG_TURN_RIGHT && averageX < MAX_X_AXIS_FOR_BIG_TURN_LEFT) {
-    digitalWrite(INVALID_TARGETS, LOW);
-    digitalWrite(TURN_LEFT, LOW);
-    digitalWrite(TURN_RIGHT, LOW);
-    digitalWrite(CENTERED, LOW);
-    digitalWrite(SMALL_TURN, LOW);
-  } else if (averageX < MAX_X_AXIS_FOR_BIG_TURN_RIGHT) {
+  if (averageX > MAX_X_AXIS_FOR_BIG_TURN_RIGHT) {
     digitalWrite(INVALID_TARGETS, LOW);
     digitalWrite(TURN_LEFT, LOW);
     digitalWrite(TURN_RIGHT, HIGH);
     digitalWrite(CENTERED, LOW);
     digitalWrite(SMALL_TURN, LOW);
-  } else if (averageX < MAX_X_AXIS_FOR_SMALL_TURN_RIGHT) {
+    Serial.println("TURN_RIGHT");
+  } else if (averageX < MAX_X_AXIS_FOR_BIG_TURN_LEFT) {
+    digitalWrite(INVALID_TARGETS, LOW);
+    digitalWrite(TURN_LEFT, HIGH);
+    digitalWrite(TURN_RIGHT, LOW);
+    digitalWrite(CENTERED, LOW);
+    digitalWrite(SMALL_TURN, LOW);
+    Serial.println("TURN_LEFT");
+  } else {
     digitalWrite(INVALID_TARGETS, LOW);
     digitalWrite(TURN_LEFT, LOW);
-    digitalWrite(TURN_RIGHT, HIGH);
-    digitalWrite(CENTERED, LOW);
-    digitalWrite(SMALL_TURN, HIGH);
-  } else if (averageX > MAX_X_AXIS_FOR_BIG_TURN_LEFT) {
-    digitalWrite(INVALID_TARGETS, LOW);
-    digitalWrite(TURN_LEFT, HIGH);
     digitalWrite(TURN_RIGHT, LOW);
-    digitalWrite(CENTERED, LOW);
-    digitalWrite(SMALL_TURN, HIGH);
-  } else if (averageX > MAX_X_AXIS_FOR_SMALL_TURN_LEFT) {
-    digitalWrite(INVALID_TARGETS, LOW);
-    digitalWrite(TURN_LEFT, HIGH);
-    digitalWrite(TURN_RIGHT, LOW);
-    digitalWrite(CENTERED, LOW);
-    digitalWrite(SMALL_TURN, HIGH);
+    digitalWrite(CENTERED, HIGH);
+    digitalWrite(SMALL_TURN, LOW);
+    Serial.println("CENTERED");
   }
 }
 
